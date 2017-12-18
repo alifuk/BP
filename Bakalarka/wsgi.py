e2eaf8f83968d3c53bccb1565f262adffcd23cbd
albert mysak <albert.mysak@tul.cz>
2017-12-18 02:09:00 +0100

    s

--- Bakalarka/wsgi.py
+++ Bakalarka/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bakalarka.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Bakalarka.settingsqqq")
 
 application = get_wsgi_application()
